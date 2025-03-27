d7210878976045fb3dedbb8c5cabe940af20b6bb
Alisha Taylor <inspirebyexample0@gmail.com>
2025-03-27 03:38:14 +0000

    Sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -10,63 +10,65 @@ import "./styles/wordscramble.css";
 import "./styles/jigsaw.css";
 import "./styles/wordsearch.css";
 
-
 export default function App() {
   return (
     <HashRouter>
       <IslamicTheme />
-      <Header />
+      <div className="app">
+        <Header />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+            <Route 
+              path="/wordscramble/:gameSlug" 
+              element={
+                <div className="game-container">
+                  <WordScrambleContainer />
+                </div>
+              } 
+            />
 
-        <Route 
-          path="/wordscramble/:gameSlug" 
-          element={
-            <div className="game-container">
-              <WordScrambleContainer />
-            </div>
-          } 
-        />
+            <Route 
+              path="/embed/wordscramble/:gameSlug" 
+              element={
+                <div className="embed-wrapper">
+                  <WordScrambleContainer />
+                </div>
+              } 
+            />
 
-        <Route 
-          path="/embed/wordscramble/:gameSlug" 
-          element={
-            <div className="embed-wrapper">
-              <WordScrambleContainer />
-            </div>
-          } 
-        />
+            <Route 
+              path="/wordsearch/:gameSlug" 
+              element={
+                <div className="game-container">
+                  <WordSearchContainer />
+                </div>
+              } 
+            />
 
-        <Route 
-          path="/wordsearch/:gameSlug" 
-          element={
-            <div className="game-container">
-              <WordSearchContainer />
-            </div>
-          } 
-        />
+            <Route 
+              path="/embed/wordsearch/:gameSlug" 
+              element={
+                <div className="embed-wrapper">
+                  <WordSearchContainer />
+                </div>
+              } 
+            />
 
-        <Route 
-          path="/embed/wordsearch/:gameSlug" 
-          element={
-            <div className="embed-wrapper">
-              <WordSearchContainer />
-            </div>
-          } 
-        />
+            <Route 
+              path="/jigsaw/:puzzleSlug" 
+              element={
+                <div className="game-container">
+                  <JigsawPuzzleContainer />
+                </div>
+              } 
+            />
 
-        <Route 
-          path="/jigsaw/:puzzleSlug" 
-          element={
-            <div className="game-container">
-              <JigsawPuzzleContainer />
-            </div>
-          } 
-        />
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </div>
     </HashRouter>
   );
 }
